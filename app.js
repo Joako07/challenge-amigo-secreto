858ffc27cb91f92df8e7f391afa09f9945718dce
Joaquin Alvarez <78284107+Joako07@users.noreply.github.com>
2025-07-30 14:28:20 -0300

    Creando Array y agregando nombres

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombreAmigos = [];
+
+function agregarNombre(){
+
+    let nuevoNombre = document.getElementById("amigo").value
+
+    nombreAmigos.push(nuevoNombre);
+     console.log(nombreAmigos);
+}
+
+
